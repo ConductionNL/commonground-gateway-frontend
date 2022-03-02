b71a62d221a283b504dba04a4df7975c0b83f8da
Sarai Misidjan <Sarai@conduction.nl>
2022-03-02 15:06:00 +0100

    file formatting

--- pwa/gatsby-config.js
+++ pwa/gatsby-config.js
@@ -1,5 +1,5 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV}`
 });
 
 module.exports = {
@@ -8,8 +8,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-layout`,
       options: {
-        component: require.resolve(`./src/components/common/layout.tsx`),
-      },
+        component: require.resolve(`./src/components/common/layout.tsx`)
+      }
     },
     {
       resolve: `gatsby-plugin-breadcrumb`,
@@ -19,67 +19,67 @@ module.exports = {
           `**/dev-404-page/**`,
           `**/404/**`,
           `**/404.html`,
-          `**/offline-plugin-app-shell-fallback/**`,
+          `**/offline-plugin-app-shell-fallback/**`
         ],
         excludeOptions: {
-          separator: ".",
+          separator: "."
         },
         autoGenHomeLabel: "Dashboard",
         crumbLabelUpdates: [
           {
-            pathname: '/sources/[id]',
-            crumbLabel: 'Source',
+            pathname: "/sources/[id]",
+            crumbLabel: "Source"
           },
           {
-            pathname: '/entities',
-            crumbLabel: 'Object Types',
+            pathname: "/entities",
+            crumbLabel: "Object Types"
           },
           {
-            pathname: '/entities/[entityId]',
-            crumbLabel: 'Object Type',
+            pathname: "/entities/[entityId]",
+            crumbLabel: "Object Type"
           },
           {
-            pathname: '/entities/[entityId]/attributes/[attributeId]',
-            crumbLabel: 'Attribute',
+            pathname: "/entities/[entityId]/attributes/[attributeId]",
+            crumbLabel: "Attribute"
           },
           {
-            pathname: '/entities/[entityId]/object_entities',
-            crumbLabel: 'Object Entities',
+            pathname: "/entities/[entityId]/object_entities",
+            crumbLabel: "Object Entities"
           },
           {
-            pathname: '/entities/[entityId]/object_entities/[objectId]',
-            crumbLabel: 'Object Entity',
+            pathname: "/entities/[entityId]/object_entities/[objectId]",
+            crumbLabel: "Object Entity"
           },
           {
-            pathname: '/endpoints/[endpointId]',
-            crumbLabel: 'Endpoint',
+            pathname: "/endpoints/[endpointId]",
+            crumbLabel: "Endpoint"
           },
           {
-            pathname: '/endpoints/[endpointId]/handlers/[handlerId]',
-            crumbLabel: 'Handler',
+            pathname: "/endpoints/[endpointId]/handlers/[handlerId]",
+            crumbLabel: "Handler"
           },
           {
-            pathname: '/applications/[id]',
-            crumbLabel: 'Application',
+            pathname: "/applications/[id]",
+            crumbLabel: "Application"
           },
           {
-            pathname: '/translation-tables',
-            crumbLabel: 'Translation Tables',
+            pathname: "/translation-tables",
+            crumbLabel: "Translation Tables"
           },
           {
-            pathname: '/translation-tables/[translationId]',
-            crumbLabel: 'Translation Table',
+            pathname: "/translation-tables/[translationId]",
+            crumbLabel: "Translation Table"
           },
           {
-            pathname: '/translation-tables/new',
-            crumbLabel: 'Translation Table',
+            pathname: "/translation-tables/new",
+            crumbLabel: "Translation Table"
           },
           {
-            pathname: '/translation-tables/[translationId]/translations/[id]',
-            crumbLabel: 'Translation',
-          },
-        ],
-      },
-    },
-  ],
+            pathname: "/translation-tables/[translationId]/translations/[id]",
+            crumbLabel: "Translation"
+          }
+        ]
+      }
+    }
+  ]
 };
