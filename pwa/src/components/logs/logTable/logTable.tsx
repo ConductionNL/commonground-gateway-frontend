c0b995c5b160bce532e361a582eaee7602e04c80
Hamid <gh.hamid89@gmail.com>
2022-03-07 16:13:40 +0100

    console error table fixt

--- pwa/src/components/logs/logTable/logTable.tsx
+++ pwa/src/components/logs/logTable/logTable.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./logTable.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye} from "@fortawesome/free-solid-svg-icons";
 import LogModal from "../logModal/LogModal";
 import LabelWithBackground from "../../LabelWithBackground/LabelWithBackground";
 import msToSeconds from "../../../services/msToSeconds";
@@ -11,7 +11,7 @@ interface LogsTableProps {
   logs: any;
 }
 
-const LogsTable: React.FC<LogsTableProps> = ({ logs }) => {
+const LogsTable: React.FC<LogsTableProps> = ({logs}) => {
   return (
     <div className="logsTable">
       <table>
@@ -23,16 +23,18 @@ const LogsTable: React.FC<LogsTableProps> = ({ logs }) => {
           <th>Response time</th>
         </tr>
         </thead>
-        {!logs.length && <tr>
+        {!logs.length && <tbody>
+        <tr>
           <td>No results found</td>
-        </tr>}
+        </tr>
+        </tbody>}
         {logs.map((log, idx) => {
           const statusClass = log.responseStatusCode > 199 && log.responseStatusCode < 300 ? "success" : "danger";
           return (
-            <>
+            <tbody>
               <tr key={idx} className="logsTable-tr">
                 <td>
-                  <LabelWithBackground label={log.responseStatusCode.toString()} type={statusClass} />
+                  <LabelWithBackground label={log.responseStatusCode.toString()} type={statusClass}/>
                 </td>
                 <td>{log.type}</td>
                 <td>{log.requestMethod}</td>
@@ -44,12 +46,12 @@ const LogsTable: React.FC<LogsTableProps> = ({ logs }) => {
                     data-bs-toggle="modal"
                     data-bs-target={`#logs${log.id.replace(new RegExp("-", "g"), "")}`}
                   >
-                    <FontAwesomeIcon icon={faEye} /> View log
+                    <FontAwesomeIcon icon={faEye}/> View log
                   </button>
                 </td>
               </tr>
-              <LogModal {...{ log }} />
-            </>
+              <LogModal {...{log}} />
+            </tbody>
           );
         })}
       </table>
